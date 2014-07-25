aa8c72f7cc1588c7009ba1c67553bbe1d7ee1dc9
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-25 16:14:21 -0400

    Update styles and add page title

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,10 +1,12 @@
 class ChatsController < ApplicationController
   def index
     @chats = current_user.chats
+    @page_title = "Your Chats"
   end
 
   def show
     @chat = Chat.find(params[:id])
+    @page_title = @chat.title
     @message = Message.new
   end
 end
